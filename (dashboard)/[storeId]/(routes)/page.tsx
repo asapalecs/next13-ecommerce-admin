cb5898f1a164c3abe7d0e2bfd010dee7bca6cbc7
Alexandru-Robert Cristinescu <101576096+asapalecs@users.noreply.github.com>
2023-07-09 15:41:15 +0300

    Dasboard Setup (Final)

--- (dashboard)/[storeId]/(routes)/page.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardPage = () =>{
-    return (
-        <div>
-            This is the dashboard
-        </div>
-    );
-}
-
-export default DashboardPage;
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
 export default function AuthLayout({
-    children  // Parameter destructuring to extract the 'children' property
-  }: {
-    children: React.ReactNode  // Type annotation for the 'children' property
-  }) {
-    return (
-      <div className="flex items-center justify-center h-full w-full"> 
-        {/* JSX code for a <div> element */}
-        {children}  {/* Rendering the child components or elements passed to the AuthLayout component */}
-      </div>
-    );
-  }
-  
+  children, // Parameter destructuring to extract the 'children' property
+}: {
+  children: React.ReactNode; // Type annotation for the 'children' property
+}) {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      {/* JSX code for a <div> element */}
+      {children}
+      {/* Rendering the child components or elements passed to the AuthLayout component */}
+    </div>
+  );
+}
